3bf59912cee0721c8ba26a5d9cdad5cc1e63b2aa
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-28 17:27:54 +0400

    feat: has been added description, what will be output for the content of outputFotStringAlerts.js

---
+++ js/outputForStringAlerts.js
@@ -0,0 +1,7 @@
+let name = "Ilya";
+
+alert(`hello ${1}`); // the output will be "hello 1"
+
+alert(`hello ${"name"}`); // the output will be "hello name"
+
+alert(`hello ${name}`); // the output will be "hello Ilya"
