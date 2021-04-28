2217f85750418037a9211c739fa3c8cba13d9e47
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-28 17:55:31 +0400

    feat: the descriptions for the expressionsResults.js file has been written

---
+++ js/expressionsResults.js
@@ -0,0 +1,7 @@
+5 > 4; //true
+"apple" > "pineapple"; //false
+"2" > "12"; //true
+undefined == null; //true
+undefined === null; //false
+null == "\n0\n"; //false, because the value of the following string is not null
+null === +"\n0\n"; //false, because the types are different
