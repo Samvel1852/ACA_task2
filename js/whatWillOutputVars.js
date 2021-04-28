38d056f49cc8896b16d6a0c050cd968fa6ded376
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-04-28 16:42:31 +0400

    feat: the i and n variables has been declared. The value of n is depend on the value of i.

---
+++ js/whatWillOutputVars.js
@@ -0,0 +1,4 @@
+let i = 10;
+let n = i++ % 5; //the output value for n will be 0 because the ++ operator gives the value
+//after that makes increment, but the value of i will be 11 because before the output
+//the ++ operator will increment the value of i.
